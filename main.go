72ca1de13892e5f5ad37eff5d3b31f269e0e7aa4
Samuel Nwoye <nwoyesamuelc@gmail.com>
2019-12-31 18:33:58 +0100

    added the go file

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Book struct {
+	isbn    string
+	title   string
+	author  string
+	price   float32
+	created time.Time
+}
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://developer:p2ssW0rd@localhost/bookstore")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	defer rows.Close()
+
+	books := make([]*Book, 0)
+	for rows.Next() {
+		book := new(Book)
+		err := rows.Scan(&book.isbn, &book.title, &book.author, &book.price, &book.created)
+		if err = rows.Err(); err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		books = append(books, book)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	for _, book := range books {
+		fmt.Fprintf(w, "%s, %s, %s, $%.2f, %s\n", book.isbn, book.title, book.author, book.price, book.created)
+	}
+
+}
+
+func booksShow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM books WHERE isbn = $1", isbn)
+
+	book := new(Book)
+	err := row.Scan(&book.isbn, &book.title, &book.author, &book.price, &book.created)
+	if err == sql.ErrNoRows {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "%s, %s, %s, $%.2f, %s\n", book.isbn, book.title, book.author, book.price, book.created)
+
+}
+
+func booksCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	title := r.FormValue("title")
+	author := r.FormValue("author")
+	if isbn == "" || title == "" || author == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	price, err := strconv.ParseFloat(r.FormValue("price"), 32)
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	created := time.Now()
+
+	result, err := db.Exec("INSERT INTO books VALUES($1, $2, $3, $4, $5)", isbn, title, author, price, created)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "Book %s created successfully (%d row affected)\n", isbn, rowsAffected)
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/books", booksIndex).Methods("GET")
+	api.HandleFunc("/books/show", booksShow).Methods("GET")
+	api.HandleFunc("/books/create", booksCreate).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
